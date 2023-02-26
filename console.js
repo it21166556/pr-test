fbce85797e0376726be5893f19a57e007e223d65
Nipuna Nadeeshan <89896805+it21166556@users.noreply.github.com>
2023-02-27 02:10:44 +0530

    Update console.js

--- console.js
+++ console.js
@@ -1,5 +1,7 @@
 //This is comment
 console.log("Hello World")
-console.log("Hello World")
-console.log("Hello World")
-console.log("Hello World")
+console.log("Hello World") // This is my 2nd pr
+
+function main() {
+return null;
+ }
